2fcd3ab042f7cf5c639f3b2a625f55d3567ac771
3837162+carlsonsantana@users.noreply.github.com <carlsonsantana@gmail.com>
2018-05-15 01:19:40 -0300

    :recycle: Separate main functions of execute functions

---
+++ execute_hatemile.php
@@ -0,0 +1,137 @@
+<?php
+
+$hatemilePath = join(DIRECTORY_SEPARATOR, array(
+    plugin_dir_path(__FILE__),
+    'hatemile_for_php',
+    'src',
+    'hatemile'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    plugin_dir_path(__FILE__),
+    'phpQuery',
+    'phpQuery',
+    'phpQuery.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'implementation',
+    'AccessibleAssociationImplementation.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'implementation',
+    'AccessibleCSSImplementation.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'implementation',
+    'AccessibleDisplayScreenReaderImplementation.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'implementation',
+    'AccessibleEventImplementation.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'implementation',
+    'AccessibleFormImplementation.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'implementation',
+    'AccessibleNavigationImplementation.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'util',
+    'Configure.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'util',
+    'css',
+    'phpcssparser',
+    'PHPCSSParser.php'
+));
+require_once join(DIRECTORY_SEPARATOR, array(
+    $hatemilePath,
+    'util',
+    'html',
+    'phpquery',
+    'PhpQueryHTMLDOMParser.php'
+));
+
+use hatemile\implementation\AccessibleAssociationImplementation;
+use hatemile\implementation\AccessibleDisplayScreenReaderImplementation;
+use hatemile\implementation\AccessibleEventImplementation;
+use hatemile\implementation\AccessibleFormImplementation;
+use hatemile\implementation\AccessibleNavigationImplementation;
+use hatemile\util\Configure;
+use hatemile\util\html\phpquery\PhpQueryHTMLDOMParser;
+
+/**
+ * Execute the HaTeMiLe.
+ * @param string $html The HTML code of page.
+ * @return string The new HTML code of page.
+ */
+function executeHatemile($html)
+{
+    try {
+        $configure = new Configure();
+        $htmlParser = new PhpQueryHTMLDOMParser($html);
+
+        $accessibleEvent = new AccessibleEventImplementation(
+            $htmlParser,
+            $configure
+        );
+        $accessibleForm = new AccessibleFormImplementation(
+            $htmlParser,
+            $configure
+        );
+        $accessibleNavigation = new AccessibleNavigationImplementation(
+            $htmlParser,
+            $configure
+        );
+        $accessibleAssociation = new AccessibleAssociationImplementation(
+            $htmlParser,
+            $configure
+        );
+        $accessibleDisplay = new AccessibleDisplayScreenReaderImplementation(
+            $htmlParser,
+            $configure
+        );
+
+        $accessibleAssociation->associateAllDataCellsWithHeaderCells();
+        $accessibleAssociation->associateAllLabelsWithFields();
+
+        $accessibleEvent->makeAccessibleAllDragandDropEvents();
+        $accessibleEvent->makeAccessibleAllClickEvents();
+        $accessibleEvent->makeAccessibleAllHoverEvents();
+
+        $accessibleForm->markAllAutoCompleteFields();
+        $accessibleForm->markAllRequiredFields();
+        $accessibleForm->markAllRangeFields();
+        $accessibleForm->markAllInvalidFields();
+
+        $accessibleDisplay->displayAllShortcuts();
+        $accessibleDisplay->displayAllRoles();
+        $accessibleDisplay->displayAllCellHeaders();
+        $accessibleDisplay->displayAllWAIARIAStates();
+        $accessibleDisplay->displayAllLinksAttributes();
+        $accessibleDisplay->displayAllTitles();
+        $accessibleDisplay->displayAllLanguages();
+        $accessibleDisplay->displayAllAlternativeTextImages();
+
+        $accessibleNavigation->provideNavigationByAllHeadings();
+        $accessibleNavigation->provideNavigationByAllSkippers();
+        $accessibleNavigation->provideNavigationToAllLongDescriptions();
+
+        $accessibleNavigation->provideNavigationByAllSkippers();
+        $accessibleDisplay->displayAllShortcuts();
+
+        return $htmlParser->getHTML();
+    } catch (Exception $exception) {
+        return $html;
+    }
+}
--- hatemile_for_wp.php
+++ hatemile_for_wp.php
@@ -13,122 +13,13 @@
  */
 function executeHatemileByBuffer()
 {
+    require_once join(DIRECTORY_SEPARATOR, array(
+        plugin_dir_path(__FILE__),
+        'execute_hatemile.php'
+    ));
     ob_start('executeHatemile');
 }
 
-/**
- * Execute the HaTeMiLe.
- * @param string $html The HTML code of page.
- * @return string The new HTML code of page.
- */
-function executeHatemile($html)
-{
-    try {
-        $hatemilePath = join(DIRECTORY_SEPARATOR, array(
-            plugin_dir_path(__FILE__),
-            'hatemile_for_php',
-            'src',
-            'hatemile'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            plugin_dir_path(__FILE__),
-            'phpQuery',
-            'phpQuery',
-            'phpQuery.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'implementation',
-            'AccessibleAssociationImplementation.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'implementation',
-            'AccessibleCSSImplementation.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'implementation',
-            'AccessibleDisplayScreenReaderImplementation.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'implementation',
-            'AccessibleEventImplementation.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'implementation',
-            'AccessibleFormImplementation.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'implementation',
-            'AccessibleNavigationImplementation.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'util',
-            'Configure.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'util',
-            'css',
-            'phpcssparser',
-            'PHPCSSParser.php'
-        ));
-        require_once join(DIRECTORY_SEPARATOR, array(
-            $hatemilePath,
-            'util',
-            'html',
-            'phpquery',
-            'PhpQueryHTMLDOMParser.php'
-        ));
-
-        $configure = new hatemile\util\Configure();
-        $htmlParser = new hatemile\util\html\phpquery\PhpQueryHTMLDOMParser($html);
-
-        $accessibleEvent = new hatemile\implementation\AccessibleEventImplementation($htmlParser, $configure);
-        $accessibleForm = new hatemile\implementation\AccessibleFormImplementation($htmlParser, $configure);
-        $accessibleNavigation = new hatemile\implementation\AccessibleNavigationImplementation($htmlParser, $configure);
-        $accessibleAssociation = new hatemile\implementation\AccessibleAssociationImplementation($htmlParser, $configure);
-        $accessibleDisplay = new hatemile\implementation\AccessibleDisplayScreenReaderImplementation($htmlParser, $configure);
-
-        $accessibleEvent->makeAccessibleAllDragandDropEvents();
-        $accessibleEvent->makeAccessibleAllClickEvents();
-        $accessibleEvent->makeAccessibleAllHoverEvents();
-
-        $accessibleForm->markAllAutoCompleteFields();
-        $accessibleForm->markAllRequiredFields();
-        $accessibleForm->markAllRangeFields();
-        $accessibleForm->markAllInvalidFields();
-
-        $accessibleNavigation->provideNavigationByAllHeadings();
-        $accessibleNavigation->provideNavigationByAllSkippers();
-        $accessibleNavigation->provideNavigationToAllLongDescriptions();
-
-        $accessibleAssociation->associateAllDataCellsWithHeaderCells();
-        $accessibleAssociation->associateAllLabelsWithFields();
-
-        $accessibleDisplay->displayAllShortcuts();
-        $accessibleDisplay->displayAllRoles();
-        $accessibleDisplay->displayAllCellHeaders();
-        $accessibleDisplay->displayAllWAIARIAStates();
-        $accessibleDisplay->displayAllLinksAttributes();
-        $accessibleDisplay->displayAllTitles();
-        $accessibleDisplay->displayAllLanguages();
-        $accessibleDisplay->displayAllAlternativeTextImages();
-
-        $accessibleNavigation->provideNavigationByAllSkippers();
-        $accessibleDisplay->displayAllShortcuts();
-
-        return $htmlParser->getHTML();
-    } catch (Exception $exception) {
-        return $html;
-    }
-}
-
 if (!is_admin()) {
-    add_action('init', 'executeHatemileByBuffer', 1);
+    add_action('init', 'executeHatemileByBuffer');
 }
